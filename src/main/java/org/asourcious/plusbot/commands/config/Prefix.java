6be4070a3be6ef3dc42758337d1ac787841f23d7
barronpm <barronpm@gmail.com>
2016-09-12 19:50:54 -0400

    Fixed prefix argument checking

--- src/main/java/org/asourcious/plusbot/commands/config/Prefix.java
+++ src/main/java/org/asourcious/plusbot/commands/config/Prefix.java
@@ -19,8 +19,10 @@ public class Prefix extends Command {
 
     @Override
     public String checkArgs(String[] args) {
-        if (args.length != 2)
+        if (args.length > 2 || args.length == 0)
             return "The Prefix command takes 2 arguments!";
+        if (args.length == 1 && !args[0].equalsIgnoreCase("clear"))
+            return "You need two arguments if you are not clearing the prefixes!";
         if (!args[0].equalsIgnoreCase("add") && !args[0].equalsIgnoreCase("remove") && !args[0].equalsIgnoreCase("clear"))
             return "The first argument can only be add or remove!";
         if (args[1].length() > 15)
