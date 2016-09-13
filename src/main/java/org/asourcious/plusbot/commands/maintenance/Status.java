09dbf19e1b77b7f25523f3bfdcf93c31d3496778
barronpm <barronpm@gmail.com>
2016-09-12 21:43:33 -0400

    Reordered status command

--- src/main/java/org/asourcious/plusbot/commands/maintenance/Status.java
+++ src/main/java/org/asourcious/plusbot/commands/maintenance/Status.java
@@ -38,22 +38,23 @@ public class Status extends Command {
         long elapsedMillis = System.currentTimeMillis() - Statistics.startTime;
 
         MessageBuilder messageBuilder = new MessageBuilder();
-        messageBuilder.appendString(PlusBot.NAME + " Status:\n");
-        messageBuilder.appendString("```xl\n");
-        messageBuilder.appendString("Name: " + PlusBot.NAME + "\n");
-        messageBuilder.appendString("Version " + PlusBot.VERSION + "\n");
-        messageBuilder.appendString("Uptime: " + FormatUtils.getFormattedTime(elapsedMillis) + "\n");
-        messageBuilder.appendString("Threads: " + Thread.activeCount() + "\n");
-        messageBuilder.appendString("CPU Usage: " + Precision.round(systemMXBean.getProcessCpuLoad() * 100, 2) + "%\n");
-        messageBuilder.appendString("RAM Usage: " + (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / 1048576 + "MB\n\n");
-        messageBuilder.appendString("JDA Info:\n");
-        messageBuilder.appendString("Guilds: " + plusBot.getShardManager().getNumberOfGuilds() + "\n");
-        messageBuilder.appendString("Text Channels: " + plusBot.getShardManager().getNumberOfTextChannels() + "\n");
-        messageBuilder.appendString("Voice Channels: " + plusBot.getShardManager().getNumberOfVoiceChannels() + "\n");
-        messageBuilder.appendString("Users: " + plusBot.getShardManager().getNumberOfUsers() + "\n");
-        messageBuilder.appendString("Connections: " + Statistics.numConnections + "\n");
-        messageBuilder.appendString("Commands: " + Statistics.numCommands + "\n");
-        messageBuilder.appendString("```");
+        messageBuilder.appendString(PlusBot.NAME + " Status:\n")
+                .appendString("```xl\n")
+                .appendString("Name: " + PlusBot.NAME + "\n")
+                .appendString("Version " + PlusBot.VERSION + "\n")
+                .appendString("Uptime: " + FormatUtils.getFormattedTime(elapsedMillis) + "\n")
+                .appendString("Threads: " + Thread.activeCount() + "\n")
+                .appendString("CPU Usage: " + Precision.round(systemMXBean.getProcessCpuLoad() * 100, 2) + "%\n")
+                .appendString("RAM Usage: " + (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / 1048576 + "MB\n")
+                .appendString("Commands: " + Statistics.numCommands + "\n")
+                .appendString("Connections: " + Statistics.numConnections + "\n\n")
+
+                .appendString("JDA Info:\n")
+                .appendString("Guilds: " + plusBot.getShardManager().getNumberOfGuilds() + "\n")
+                .appendString("Text Channels: " + plusBot.getShardManager().getNumberOfTextChannels() + "\n")
+                .appendString("Voice Channels: " + plusBot.getShardManager().getNumberOfVoiceChannels() + "\n")
+                .appendString("Users: " + plusBot.getShardManager().getNumberOfUsers() + "\n")
+                .appendString("```");
 
         event.getChannel().sendMessageAsync(messageBuilder.build(), null);
     }
