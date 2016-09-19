2a7b55116fe7d38152500c6e449a442e6ec575c3
barronpm <barronpm@gmail.com>
2016-09-19 19:21:09 -0400

    Updated version to 1.2

--- src/main/java/org/asourcious/plusbot/PlusBot.java
+++ src/main/java/org/asourcious/plusbot/PlusBot.java
@@ -51,7 +51,7 @@ import java.util.concurrent.TimeUnit;
 public class PlusBot {
 
     public static final String NAME = "PlusBot";
-    public static final String VERSION = "1.1";
+    public static final String VERSION = "1.2";
     public static final String OWNER_ID = "142843635057164288";
 
     public static final SimpleLog LOG = SimpleLog.getLog("PlusBot");
