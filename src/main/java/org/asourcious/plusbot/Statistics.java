819cac0032e566162f7d0433385a1d382bbe45e3
barronpm <barronpm@gmail.com>
2016-09-19 21:19:55 -0400

    Cleaned up Statistics

--- src/main/java/org/asourcious/plusbot/Statistics.java
+++ src/main/java/org/asourcious/plusbot/Statistics.java
@@ -1,12 +1,11 @@
 package org.asourcious.plusbot;
 
-
 import java.time.OffsetDateTime;
 
 public final class Statistics {
     private Statistics() {}
 
     public static OffsetDateTime startTime;
-    public static long numCommands = 0;
-    public static long numConnections = 0;
+    public static int numCommands = 0;
+    public static int numConnections = 0;
 }
